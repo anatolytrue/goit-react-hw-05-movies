ee74abc48fe388d14464bbdc776838dca17134cc
Anatolii <anatoly.tomnyak@gmail.com>
2022-10-13 00:52:27 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
-export const App = () => {
+import { Routes, Route } from 'react-router-dom'
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path='/' element={ <div>Test</div>} />
+      </Routes>
     </div>
-  );
-};
+  )
+}
