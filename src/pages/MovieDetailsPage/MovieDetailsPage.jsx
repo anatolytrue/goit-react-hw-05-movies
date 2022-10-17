b3b9a8e8a793937709037cae301f2d14a6152f50
Anatolii <anatoly.tomnyak@gmail.com>
2022-10-17 18:50:43 +0300

    Update MovieDetailsPage.jsx

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -8,6 +8,7 @@ const MoviesDetailsPage = () => {
         <h2>Now showing product - {movie.name} with id - {id}</h2>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
             Neque, esse hic quas quam unde quaerat.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
     </div>
 };
 
